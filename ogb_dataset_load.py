38374762ef68a92ec8ec3aa0c7e11c7aaf473f62
Saurabh Bajaj <saurabh.rb440@gmail.com>
2023-02-23 01:02:46 +0000

    not committing datasets; temporary test file to load a ogb dataset

---
+++ ogb_dataset_load.py
@@ -0,0 +1,10 @@
+from ogb.graphproppred import PygGraphPropPredDataset
+from torch_geometric.loader import DataLoader
+
+# Download and process data at './dataset/ogbg_molhiv/'
+dataset = PygGraphPropPredDataset(name = 'ogbg-molhiv')
+
+split_idx = dataset.get_idx_split() 
+train_loader = DataLoader(dataset[split_idx['train']], batch_size=32, shuffle=True)
+valid_loader = DataLoader(dataset[split_idx['valid']], batch_size=32, shuffle=False)
+test_loader = DataLoader(dataset[split_idx['test']], batch_size=32, shuffle=False)
