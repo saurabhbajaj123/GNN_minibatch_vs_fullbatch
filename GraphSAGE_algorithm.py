4e34d87dd7d46f3ec2425d5f5a715c151237ea5d
Saurabh Bajaj <saurabh.rb440@gmail.com>
2023-02-23 03:11:51 +0000

    Graph SAGE algorithm class

---
+++ GraphSAGE_algorithm.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import SAGEConv
+class SAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers=3):
+        super(SAGE, self).__init__()
+
+        self.num_layers = num_layers
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden_channels, hidden_channels))
+        self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, adjs):
+        # `train_loader` computes the k-hop neighborhood of a batch of nodes,
+        # and returns, for each layer, a bipartite graph object, holding the
+        # bipartite edges `edge_index`, the index `e_id` of the original edges,
+        # and the size/shape `size` of the bipartite graph.
+        # Target nodes are also included in the source nodes so that one can
+        # easily apply skip-connections or add self-loops.
+        for i, (edge_index, _, size) in enumerate(adjs):
+            xs = []
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x, x_target), edge_index)
+            if i != self.num_layers - 1:
+                x = F.relu(x)
+                x = F.dropout(x, p=0.5, training=self.training)
+            xs.append(x)
+            if i == 0: 
+                x_all = torch.cat(xs, dim=0)
+                layer_1_embeddings = x_all
+            elif i == 1:
+                x_all = torch.cat(xs, dim=0)
+                layer_2_embeddings = x_all
+            elif i == 2:
+                x_all = torch.cat(xs, dim=0)
+                layer_3_embeddings = x_all    
+        #return x.log_softmax(dim=-1)
+        return layer_1_embeddings, layer_2_embeddings, layer_3_embeddings
+
+    def inference(self, x_all):
+        pbar = tqdm(total=x_all.size(0) * self.num_layers)
+        pbar.set_description('Evaluating')
+
+        # Compute representations of nodes layer by layer, using *all*
+        # available edges. This leads to faster computation in contrast to
+        # immediately computing the final representations of each batch.
+        total_edges = 0
+        for i in range(self.num_layers):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                total_edges += edge_index.size(1)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = self.convs[i]((x, x_target), edge_index)
+                if i != self.num_layers - 1:
+                    x = F.relu(x)
+                xs.append(x)
+
+                pbar.update(batch_size)
+
+            if i == 0: 
+                x_all = torch.cat(xs, dim=0)
+                layer_1_embeddings = x_all
+            elif i == 1:
+                x_all = torch.cat(xs, dim=0)
+                layer_2_embeddings = x_all
+            elif i == 2:
+                x_all = torch.cat(xs, dim=0)
+                layer_3_embeddings = x_all
+                
+        pbar.close()
+
+        return layer_1_embeddings, layer_2_embeddings, layer_3_embeddings
