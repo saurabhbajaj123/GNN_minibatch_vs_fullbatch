561936983be214a352384c4ac40c101978b078f3
Ubuntu <ubuntu@ip-172-31-79-39.ec2.internal>
2023-03-29 21:14:44 +0000

    install nvtop

--- installations.sh
+++ installations.sh
@@ -3,3 +3,4 @@ pip install  dgl -f https://data.dgl.ai/wheels/cu117/repo.html
 pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
 pip install ogb
 pip install wandb
+sudo apt install nvtop
