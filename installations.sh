c4957c1b42cabba7f25e261d603272b7c552b98e
Ubuntu <ubuntu@ip-172-31-23-50.ec2.internal>
2023-06-19 05:05:59 +0000

    updated version of DGL

--- installations.sh
+++ installations.sh
@@ -1,5 +1,5 @@
 pip3 install torch torchvision torchaudio
-pip install  dgl -f https://data.dgl.ai/wheels/cu117/repo.html
+pip install  dgl -f https://data.dgl.ai/wheels/cu118/repo.html
 pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
 pip install ogb
 pip install wandb
