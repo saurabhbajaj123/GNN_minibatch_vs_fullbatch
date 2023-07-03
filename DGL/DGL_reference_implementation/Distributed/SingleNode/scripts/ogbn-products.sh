3e3f83b31c12ba3127d26e11004634daf545c1e8
Saurabh <saurabh.rb440@gmail.com>
2023-07-03 13:45:47 +0000

    bash file with current experimental parameters

--- DGL/DGL_reference_implementation/Distributed/SingleNode/scripts/ogbn-products.sh
+++ DGL/DGL_reference_implementation/Distributed/SingleNode/scripts/ogbn-products.sh
@@ -2,15 +2,15 @@ python3 main.py \
   --dataset ogbn-products \
   --dropout 0.3 \
   --lr 0.003 \
-  --n-gpus 4 \
-  --n-epochs 6 \
+  --n-gpus 1 \
+  --n-epochs 50 \
   --model graphsage \
   --log-every 5 \
-  --n-layers 3 \
+  --n-layers 4 \
   --n-hidden 128 \
-  --batch-size 2048 \
+  --batch-size 1024 \
   --fanout 4 \
-  --agg mean \
+  --agg gcn \
   # --use-pp \
   # --enable-pipeline \
   # --fix-seed \
