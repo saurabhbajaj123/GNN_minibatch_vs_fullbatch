f1d026b03cb2de9781329b243f3e4c424cdadd15
Ubuntu <ubuntu@ip-172-31-68-93.ec2.internal>
2023-07-08 08:41:29 +0000

    hyper parameters set and trial run for multi gpu

--- DGL/DGL_reference_implementation/Distributed/MultiGPU/scripts/ogbn-products.sh
+++ DGL/DGL_reference_implementation/Distributed/MultiGPU/scripts/ogbn-products.sh
@@ -1,14 +1,17 @@
 python main.py \
   --dataset ogbn-products \
+  --model graphsage \
+  --sampling NS \
   --dropout 0.3 \
-  --lr 0.003 \
-  --n-epochs 10 \
+  --lr 0.001 \
+  --n-epochs 50 \
   --n-gpus 4 \
   --n-layers 3 \
-  --n-hidden 728 \
+  --n-hidden 256 \
   --batch-size 1024 \
-  --fanout 10 \
+  --fanout 4 \
   --agg mean \
   --log-every 5 \
+#   --seed 10245829 \
 #   --mode puregpu \
   # --seed \
