7ca0bf98d9669418ba98eb363fb9854bf5a279c6
Ubuntu <ubuntu@ip-172-31-7-231.ec2.internal>
2023-07-27 17:50:40 +0000

    commenting out pipeGCN and reducing epochs for pubmed

--- train_time_pipegcn_multigpu.sh
+++ train_time_pipegcn_multigpu.sh
@@ -1,122 +1,122 @@
-#vanilla
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset ogbn-arxiv \
-  --dropout 0.3 \
-  --lr 0.003 \
-  --n-partitions 4 \
-  --n-epochs 2000 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 859 \
-  --log-every 5 \
-  --fix-seed \
-  --use-pp \
-  --seed 1261325436 \
+# #vanilla
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset ogbn-arxiv \
+#   --dropout 0.3 \
+#   --lr 0.003 \
+#   --n-partitions 4 \
+#   --n-epochs 2000 \
+#   --model graphsage \
+#   --n-layers 4 \
+#   --n-hidden 859 \
+#   --log-every 5 \
+#   --fix-seed \
+#   --use-pp \
+#   --seed 1261325436 \
 
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset pubmed \
-  --dropout 0.7565688403188127 \
-  --lr 0.0001 \
-  --n-partitions 4 \
-  --n-epochs 1000 \
-  --model graphsage \
-  --n-layers 3 \
-  --n-hidden 187 \
-  --log-every 5 \
-  --use-pp \
-  --fix-seed \
-  --seed 1344439319 \
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset pubmed \
+#   --dropout 0.7565688403188127 \
+#   --lr 0.0001 \
+#   --n-partitions 4 \
+#   --n-epochs 1000 \
+#   --model graphsage \
+#   --n-layers 3 \
+#   --n-hidden 187 \
+#   --log-every 5 \
+#   --use-pp \
+#   --fix-seed \
+#   --seed 1344439319 \
 
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset ogbn-products \
-  --dropout 0.3 \
-  --lr 0.003 \
-  --n-partitions 4 \
-  --n-epochs 500 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 127 \
-  --log-every 5 \
-  --use-pp \
-  --fix-seed \
-  --seed 837330801 \
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset ogbn-products \
+#   --dropout 0.3 \
+#   --lr 0.003 \
+#   --n-partitions 4 \
+#   --n-epochs 500 \
+#   --model graphsage \
+#   --n-layers 4 \
+#   --n-hidden 127 \
+#   --log-every 5 \
+#   --use-pp \
+#   --fix-seed \
+#   --seed 837330801 \
 
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset reddit \
-  --dropout 0.3 \
-  --lr 0.001 \
-  --n-partitions 4 \
-  --n-epochs 1000 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 312 \
-  --log-every 5 \
-  --fix-seed \
-  --seed 1586505639 \
-  --use-pp \
-
-
-
-# enable pipeline on
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset ogbn-arxiv \
-  --dropout 0.3 \
-  --lr 0.003 \
-  --n-partitions 4 \
-  --n-epochs 2000 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 859 \
-  --log-every 5 \
-  --fix-seed \
-  --seed 1261325436 \
-  --enable-pipeline \
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset reddit \
+#   --dropout 0.3 \
+#   --lr 0.001 \
+#   --n-partitions 4 \
+#   --n-epochs 1000 \
+#   --model graphsage \
+#   --n-layers 4 \
+#   --n-hidden 312 \
+#   --log-every 5 \
+#   --fix-seed \
+#   --seed 1586505639 \
+#   --use-pp \
 
 
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset pubmed \
-  --dropout 0.7565688403188127 \
-  --lr 0.0001 \
-  --n-partitions 4 \
-  --n-epochs 1000 \
-  --model graphsage \
-  --n-layers 3 \
-  --n-hidden 187 \
-  --log-every 5 \
-  --use-pp \
-  --fix-seed \
-  --seed 1344439319 \
-  --enable-pipeline \
+
+# # enable pipeline on
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset ogbn-arxiv \
+#   --dropout 0.3 \
+#   --lr 0.003 \
+#   --n-partitions 4 \
+#   --n-epochs 2000 \
+#   --model graphsage \
+#   --n-layers 4 \
+#   --n-hidden 859 \
+#   --log-every 5 \
+#   --fix-seed \
+#   --seed 1261325436 \
+#   --enable-pipeline \
 
 
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset ogbn-products \
-  --dropout 0.3 \
-  --lr 0.001 \
-  --n-partitions 4 \
-  --n-epochs 1000 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 127 \
-  --log-every 5 \
-  --use-pp \
-  --fix-seed \
-  --seed 22978128 \
-  --enable-pipeline \
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset pubmed \
+#   --dropout 0.7565688403188127 \
+#   --lr 0.0001 \
+#   --n-partitions 4 \
+#   --n-epochs 1000 \
+#   --model graphsage \
+#   --n-layers 3 \
+#   --n-hidden 187 \
+#   --log-every 5 \
+#   --use-pp \
+#   --fix-seed \
+#   --seed 1344439319 \
+#   --enable-pipeline \
 
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
-  --dataset reddit \
-  --dropout 0.3 \
-  --lr 0.001 \
-  --n-partitions 4 \
-  --n-epochs 1000 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 312 \
-  --log-every 5 \
-  --fix-seed \
-  --seed 1586505639 \
-  --enable-pipeline \
+
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset ogbn-products \
+#   --dropout 0.3 \
+#   --lr 0.001 \
+#   --n-partitions 4 \
+#   --n-epochs 1000 \
+#   --model graphsage \
+#   --n-layers 4 \
+#   --n-hidden 127 \
+#   --log-every 5 \
+#   --use-pp \
+#   --fix-seed \
+#   --seed 22978128 \
+#   --enable-pipeline \
+
+# python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#   --dataset reddit \
+#   --dropout 0.3 \
+#   --lr 0.001 \
+#   --n-partitions 4 \
+#   --n-epochs 1000 \
+#   --model graphsage \
+#   --n-layers 4 \
+#   --n-hidden 312 \
+#   --log-every 5 \
+#   --fix-seed \
+#   --seed 1586505639 \
+#   --enable-pipeline \
 
 
 
@@ -130,7 +130,7 @@ python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/DGL/DGL_referenc
   --lr 0.001 \
   --n-epochs 1000 \
   --n-gpus 4 \
-  --n-layers 3 \
+  --n-layers 9 \
   --n-hidden 256 \
   --batch-size 1024 \
   --fanout 4 \
@@ -149,7 +149,7 @@ python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/DGL/DGL_referenc
   --lr 0.001 \
   --n-epochs 500 \
   --n-gpus 4 \
-  --n-layers 3 \
+  --n-layers 5 \
   --n-hidden 256 \
   --batch-size 512 \
   --fanout 4 \
@@ -166,7 +166,7 @@ python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/DGL/DGL_referenc
   --sampling NS \
   --dropout 0.3 \
   --lr 0.001 \
-  --n-epochs 1000 \
+  --n-epochs 300 \
   --n-gpus 4 \
   --n-layers 4 \
   --n-hidden 256 \
@@ -185,9 +185,9 @@ python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/DGL/DGL_referenc
   --sampling NS \
   --dropout 0.3 \
   --lr 0.001 \
-  --n-epochs 1000 \
+  --n-epochs 500 \
   --n-gpus 4 \
-  --n-layers 3 \
+  --n-layers 7 \
   --n-hidden 256 \
   --batch-size 1024 \
   --fanout 4 \
