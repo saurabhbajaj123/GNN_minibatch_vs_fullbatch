e87688beec12a6af3eba81f482dec5c501b773fa
Ubuntu <ubuntu@ip-172-31-7-231.ec2.internal>
2023-07-31 18:35:46 +0000

    creating multipleruns script for cluster multigpu sage

---
+++ DGL/DGL_reference_implementation/Distributed/MultiGPU/cluster_scripts/multiple_runs.sh
@@ -0,0 +1,73 @@
+python cluster_main.py \
+  --dataset ogbn-products \
+  --model graphsage \
+  --sampling cluster \
+  --dropout 0.3 \
+  --lr 0.002 \
+  --n-epochs 500 \
+  --n-gpus 4 \
+  --n-layers 4 \
+  --n-hidden 728 \
+  --batch-size 512 \
+  --num-partitions 4000 \
+  --agg mean \
+  --log-every 5 \
+  --seed 3485963027166655500 \
+#   --mode puregpu \
+  # --seed \
+
+
+python cluster_main.py \
+  --dataset reddit \
+  --model graphsage \
+  --sampling cluster \
+  --dropout 0.3 \
+  --lr 0.003 \
+  --n-epochs 1000 \
+  --n-gpus 4 \
+  --n-layers 4 \
+  --n-hidden 728 \
+  --batch-size 1024 \
+  --num-partitions 4000 \
+  --agg mean \
+  --log-every 5 \
+  --seed 42 \
+#   --mode puregpu \
+  # --seed \
+
+python cluster_main.py \
+  --dataset ogbn-arxiv \
+  --model graphsage \
+  --sampling cluster \
+  --dropout 0.3 \
+  --lr 0.001 \
+  --n-epochs 1000 \
+  --n-gpus 4 \
+  --n-layers 6 \
+  --n-hidden 512 \
+  --batch-size 1024 \
+  --num-partitions 4000 \
+  --agg mean \
+  --log-every 5 \
+#   --seed 10245829 \
+#   --mode puregpu \
+  # --seed \
+
+
+python cluster_main.py \
+  --dataset pubmed \
+  --model graphsage \
+  --sampling cluster \
+  --dropout 0.3 \
+  --lr 0.003 \
+  --n-epochs 500 \
+  --n-gpus 4 \
+  --n-layers 10 \
+  --n-hidden 256 \
+  --batch-size 1024 \
+  --num-partitions 2000 \
+  --agg mean \
+  --log-every 5 \
+#   --seed 10245829 \
+#   --mode puregpu \
+  # --seed \
