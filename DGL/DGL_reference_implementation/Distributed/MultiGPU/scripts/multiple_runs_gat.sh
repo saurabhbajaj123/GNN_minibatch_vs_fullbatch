8d5b3f490e471c0e37637fdccf858066027c1987
Ubuntu <ubuntu@ip-172-31-70-126.ec2.internal>
2023-08-31 10:02:44 +0000

    HPO

--- DGL/DGL_reference_implementation/Distributed/MultiGPU/scripts/multiple_runs_gat.sh
+++ DGL/DGL_reference_implementation/Distributed/MultiGPU/scripts/multiple_runs_gat.sh
@@ -67,22 +67,22 @@
 # running all with 0.001 lr
 
 
-python main.py \
-  --dataset pubmed \
-  --model gat \
-  --sampling NS \
-  --dropout 0.3 \
-  --lr 0.0005 \
-  --n-epochs 200 \
-  --n-gpus 4 \
-  --n-layers 4 \
-  --n-hidden 256 \
-  --num-heads 2 \
-  --batch-size 2048 \
-  --fanout 7 \
-  --agg mean \
-  --log-every 5 \
-  --seed 42
+# python main.py \
+#   --dataset pubmed \
+#   --model gat \
+#   --sampling NS \
+#   --dropout 0.3 \
+#   --lr 0.0005 \
+#   --n-epochs 200 \
+#   --n-gpus 4 \
+#   --n-layers 4 \
+#   --n-hidden 256 \
+#   --num-heads 2 \
+#   --batch-size 2048 \
+#   --fanout 7 \
+#   --agg mean \
+#   --log-every 5 \
+#   --seed 42
 
 
 python main.py \
@@ -91,13 +91,13 @@ python main.py \
   --sampling NS \
   --dropout 0.3 \
   --lr 0.0005 \
-  --n-epochs 200 \
+  --n-epochs 2000 \
   --n-gpus 4 \
-  --n-layers 3 \
+  --n-layers 5 \
   --n-hidden 256 \
   --num-heads 2 \
   --batch-size 2048 \
-  --fanout 15 \
+  --fanout 10 \
   --agg mean \
   --log-every 5 \
   --seed 42
@@ -108,10 +108,10 @@ python main.py \
   --sampling NS \
   --dropout 0.3 \
   --lr 0.0005 \
-  --n-epochs 200 \
+  --n-epochs 2000 \
   --n-gpus 4 \
-  --n-layers 3 \
-  --n-hidden 256 \
+  --n-layers 2 \
+  --n-hidden 128 \
   --num-heads 2 \
   --batch-size 2048 \
   --fanout 10 \
@@ -125,13 +125,13 @@ python main.py \
   --sampling NS \
   --dropout 0.3 \
   --lr 0.0005 \
-  --n-epochs 200 \
+  --n-epochs 2000 \
   --n-gpus 4 \
   --n-layers 3 \
-  --n-hidden 256 \
+  --n-hidden 64 \
   --num-heads 2 \
   --batch-size 2048 \
-  --fanout 4 \
+  --fanout 7 \
   --agg mean \
   --log-every 5 \
   --seed 42 
