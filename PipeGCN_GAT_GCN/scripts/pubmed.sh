9958a82eb4eaf7f82acd961d3196c30b7b9c0fa7
Saurabh Bajaj <saurabh.rb440@gmail.com>
2023-09-19 21:02:51 +0000

    scripts for pipegcn gat

--- PipeGCN_GAT_GCN/scripts/pubmed.sh
+++ PipeGCN_GAT_GCN/scripts/pubmed.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name pubmed   ## name that will show up in the queue
+#SBATCH -p gpu-preempt
+#SBATCH --gpus=4
+#SBATCH --mem=20GB  # memory per CPU core
+#SBATCH --time=0-00:30:00  ## time for analysis (day-hour:min:sec)
+#SBATCH --constraint=m40
+#SBATCH --output=result.txt
+
+
+source /work/sbajaj_umass_edu/GNNEnv/bin/activate
+
 python main.py \
   --dataset pubmed \
   --dropout 0.5 \
@@ -5,9 +18,9 @@ python main.py \
   --n-partitions 4 \
   --n-epochs 1000 \
   --model gat \
-  --num_heads 2 \
-  --n-layers 3 \
-  --n-hidden 187 \
+  --num-heads 2 \
+  --n-layers 2 \
+  --n-hidden 128 \
   --log-every 5 \
   --use-pp \
   --enable-pipeline \
--- PipeGCN_GAT_GCN/scripts/reddit.sh
+++ PipeGCN_GAT_GCN/scripts/reddit.sh
@@ -1,12 +1,25 @@
-python /home/ubuntu/GNN_mini_vs_full/GNN_minibatch_vs_fullbatch/PipeGCN/main.py \
+#!/bin/bash
+
+#SBATCH --job-name ogbn-products   ## name that will show up in the queue
+#SBATCH -p gpu-preempt
+#SBATCH --gpus=4
+#SBATCH --mem=50GB  # memory per CPU core
+#SBATCH --time=0-00:30:00  ## time for analysis (day-hour:min:sec)
+#SBATCH --output=result.txt
+
+
+source /work/sbajaj_umass_edu/GNNEnv/bin/activate
+
+python main.py \
   --dataset reddit \
   --dropout 0.3 \
   --lr 0.001 \
   --n-partitions 4 \
   --n-epochs 1000 \
-  --model graphsage \
-  --n-layers 4 \
-  --n-hidden 312 \
+  --model gat \
+  --n-layers 2 \
+  --n-hidden 128 \
+  --num-heads 3 \
   --log-every 5 \
   --fix-seed \
   --seed 1586505639 \
